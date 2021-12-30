b334ca75f430584f8d6967f9c9384e9fa23f9556
Your Name <you@example.com>
2021-12-30 23:09:45 +0100

    'first'

---
+++ app/screens/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Login(props) {
+    return (
+        <View style={styles.background}>
+
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#172B4D',
+    },
+});
+
+export default Login;
